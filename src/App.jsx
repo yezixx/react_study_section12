cbb321ca8ebdf49a107ff8687ca3156ca4b64917
yezixx <skylow917@daum.net>
2024-06-24 22:06:04 +0900

    12.6 폰트, 이미지, 레이아웃 설정

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import New from "./pages/New";
 import Diary from "./pages/Diary";
 import NotFound from "./pages/NotFound";
 
+import { getEmotionImage } from "./util/get-emotion-image";
+
 function App() {
   const nav = useNavigate();
 
@@ -13,6 +15,13 @@ function App() {
 
   return (
     <>
+      <div>
+        <img src={getEmotionImage(1)} />
+        <img src={getEmotionImage(2)} />
+        <img src={getEmotionImage(3)} />
+        <img src={getEmotionImage(4)} />
+        <img src={getEmotionImage(5)} />
+      </div>
       <div>
         <Link to={"/"}>Home</Link>
         <Link to={"/new"}>New</Link>
